56f6bf220e1549feed2638ff112b97a0cdc8ec8a
Shuaib Khan <shuaib.middle@gmail.com>
2024-02-01 22:40:48 +0500

    updated

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 8000;
+const mongoose = require('mongoose');
+const port = process.env.PORT;
+const Item = require('./model');
 
-// Sample data (replace with your data)
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch((err) => {
+    console.error("DB CONNECTION FAILED", err);
+  });
+
+
+
+
+
 const items = [
   { id: 1, name: 'Item 1' },
   { id: 2, name: 'Item 2' },
@@ -13,9 +29,33 @@ const items = [
 // Middleware to parse JSON requests
 app.use(express.json());
 
+app.post('/api/items', async (req, res) => {
+  try {
+    const { name, description } = req.body;
+
+    if (!name || !description) {
+      return res.status(400).json({ error: 'Name and description are required' });
+    }
+
+    const newItem = new Item({ name, description });
+    await newItem.save();
+
+    res.status(201).json(newItem);
+  } catch (error) {
+    console.error('Error creating item:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 // Define a route to get a list of items
-app.get('/api/items', (req, res) => {
-  res.json(items);
+app.get('/api/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (error) {
+    console.error('Error fetching items:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 // Start the server
